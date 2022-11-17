29693c3d8baf24a7edf71bc6bf5e67e5db96a181
muammar.fikri <muammar.fikri@traveloka.com>
2022-11-17 21:00:25 +0700

    test

--- app/src/main/java/com/playground/kspplayground/SampleActivity2.kt
+++ app/src/main/java/com/playground/kspplayground/SampleActivity2.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.playground.ksp_annotation.AnnotateMe
 
+@AnnotateMe
 class SampleActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
